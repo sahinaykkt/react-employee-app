a5548779888c0ba496092c66b28e1f3c828f806f
Aykut Şahin <aykut.sahin@insider.local>
2023-01-31 01:46:26 +0300

    The general structure of the project was created.

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { EmployeeContext } from '../context/EmployeeContext';
 import Employee from './Employee';
 
 function EmployeeList() {
+  const { employees } = useContext(EmployeeContext);
+
   return (
-    <div>
-        <Employee />
-    </div>
+    <table className="table table-striped table-hover">
+        <thead>
+            <tr>
+                <th>Name</th> 
+                <th>Email</th>
+                <th>Address</th>
+                <th>Phone</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            <Employee employees={employees}/>
+        </tbody>
+    </table>
   )
 }
 
-export default EmployeeList
+export default EmployeeList;
