29ff53d7ab6e2a2f1c6351447b920cb8166a7ea7
sahinaykkt <sahinaykkt@gmail.com>
2023-01-31 01:54:36 +0300

    Minor update.

--- src/context/EmployeeContext.js
+++ src/context/EmployeeContext.js
@@ -6,7 +6,7 @@ export const EmployeeContext = createContext();
 const EmployeeContextProvider = (props) => {
     
     const [employees, setEmployees] = useState([
-        {id:uuidv4(), name: 'Thomas Hardy', email: 'thomas.hardy@mail.com', address: '89 Chiaroscuro Rd, Portland, USA', phone: '(171) 555-2222'},
+        {id:uuidv4(), name: 'Thomas Hardy', email: 'thomashardy@mail.com', address: '89 Chiaroscuro Rd, Portland, USA', phone: '(171) 555-2222'},
         {id:uuidv4(), name: 'Dominique Perrier', email: 'dominiqueperrier@mail.com', address: 'Obere Str. 57, Berlin, Germany', phone: '(313) 555-5735'},
         {id:uuidv4(), name: 'Maria Anders', email: 'mariaanders@mail.com', address: '25, rue Lauriston, Paris, France', phone: '(503) 555-9931'},
         {id:uuidv4(), name: 'Fran Wilson', email: 'franwilson@mail.com', address: 'C/ Araquil, 67, Madrid, Spain', phone: '(204) 619-5731'},
